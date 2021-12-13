832493c90b5732e0c40456eb3d29573a91b98659
ad <ad@DESKTOP-7RNKS49>
2021-12-13 18:58:22 +0700

    Add project files.

---
+++ baitaplapthay/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_BT1
+{
+    internal class Program
+    {
+        static void Main(string[]args)
+        {
+            int a, b;
+            Console.WriteLine("Cho vao a bang");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("cho vao b bang");
+            b=Convert.ToInt32(Console.ReadLine());
+            if (b == 0)
+            {
+                Console.WriteLine("Error: divided by zero");
+            }
+            else
+            {
+                float c = (float)a / b;
+                Console.WriteLine("c=" + c);
+            }
+            Console.ReadKey();
+        }
+          
+    }
+}
+
+
